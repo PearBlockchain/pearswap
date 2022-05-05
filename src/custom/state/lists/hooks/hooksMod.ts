d799be00e6b1372ccd3b259c23cacc18be174b63
Samuel Safahi <samsafahi@gmail.com>
2022-05-05 12:00:44 -0700

    Revert "Update hooksMod.ts"
    This reverts commit c464d6984bc1755aba1e1efd67d105dd72a2481f.

--- src/custom/state/lists/hooks/hooksMod.ts
+++ src/custom/state/lists/hooks/hooksMod.ts
@@ -40,6 +40,7 @@ export const EMPTY_LIST: TokenAddressMap = {
   [ChainId.RINKEBY]: {},
   [ChainId.MAINNET]: {},
   [ChainId.XDAI]: {},
+  [ChainId.CANDLE]: {},
 }
 
 const listCache: WeakMap<TokenList, TokenAddressMap> | null =
